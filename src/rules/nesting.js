b9b8ab54b4874bcd1608f12f9c1d9019cc44e73c
Jesse Shawl <jesse@jshawl.com>
2013-12-26 22:42:34 -0500

    Add nesting rule

---
+++ src/rules/nesting.js
@@ -0,0 +1,11 @@
+/*
+ * Rule: Don't nest more than three selectors  
+ */
+
+grass.addRule({
+  
+    id: "nesting",
+    name: "Disallow nesting more than three selectors",
+    desc: "Disallow nesting more than three selectors"
+
+});
